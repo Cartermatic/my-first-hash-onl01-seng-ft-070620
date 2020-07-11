4ea6684a00968df9ec408aabf107d5749f79aa0a
Carter Parrett <cparrett94@hotmail.com>
2020-07-11 02:56:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,11 @@
 def my_hash
  my_hash = { "ashe" => "ADC", "ahri" => "Mid" }
-  
 end
 
 
-# def shipping_manifest
-#   the_manifest = { "whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3" }
-# end
+def shipping_manifest
+  the_manifest = { "whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3" }
+end
 
 
 end
